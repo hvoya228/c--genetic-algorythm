f7f632366536dce6e0bef9220fee6405c3590789
Chugaister <bogucukij@gmail.com>
2023-12-05 19:09:58 +0200

    initial

---
+++ GeneticAlgorythm/GeneticAlgorythm.cs
@@ -0,0 +1,56 @@
+﻿public class GeneticAlgorythm
+{
+    static Random random = new Random();
+
+    public static double FitnessFunction(double x)
+    {
+        // Функція для максимізації (f(x) = 2x^2 + 1)
+        return 2 * Math.Pow(x, 2) + 1;
+    }
+
+    public static string DecimalToBinary(int decimalNumber, int numberOfBits)
+    {
+        // Переведення десяткового числа у двійковий код
+        return Convert.ToString(decimalNumber, 2).PadLeft(numberOfBits, '0');
+    }
+
+    public static double BinaryToDecimal(string binaryNumber)
+    {
+        // Переведення двійкового коду у десяткове число
+        return Convert.ToInt32(binaryNumber, 2);
+    }
+
+    public static List<string> InitializePopulation(int populationSize, int numberOfBits, int chromosomeSize)
+    {
+        // Створення початкової популяції
+        List<string> population = new List<string>();
+        for (int i = 0; i < populationSize; i++)
+        {
+            int randomDecimal = random.Next(0, (int)Math.Pow(2, numberOfBits));
+            string binaryRepresentation = DecimalToBinary(randomDecimal, numberOfBits);
+            population.Add(binaryRepresentation.PadLeft(chromosomeSize * numberOfBits, '0'));
+        }
+        return population;
+    }
+
+    public static string Crossover(string parent1, string parent2)
+    {
+        // Рандомний кросовер
+        int crossoverPoint = random.Next(1, parent1.Length);
+        return parent1.Substring(0, crossoverPoint) + parent2.Substring(crossoverPoint);
+    }
+
+    public static void Mutate(ref string individual, double mutationRate)
+    {
+        // Мутація генів
+        char[] genes = individual.ToCharArray();
+        for (int i = 0; i < genes.Length; i++)
+        {
+            if (random.NextDouble() < mutationRate)
+            {
+                genes[i] = (genes[i] == '0') ? '1' : '0'; // Зміна значення гена
+            }
+        }
+        individual = new string(genes);
+    }
+}
---
+++ GeneticAlgorythm/Program.cs
@@ -0,0 +1,61 @@
+﻿
+int populationSize = 50; // кількість індивідумів у популяції
+int numberOfBits = 8; // кількість символів в одній хромосомі
+int chromosomeSize = 4; // кількість хромосом в індивідума
+int generations = 100; // кількість поколінь
+double mutationRate = 0.01; // ймовірність мутації
+
+// Ініціалізація початкової популяції
+List<string> population = GeneticAlgorythm.InitializePopulation(populationSize, numberOfBits, chromosomeSize);
+
+for (int generation = 0; generation < generations; generation++)
+{
+    // Створення нової популяції
+    List<string> newPopulation = new List<string>();
+    for (int i = 0; i < populationSize; i += 2)
+    {
+        // Вибір батьків
+        string parent1 = population[i];
+        string parent2 = population[i + 1];
+
+        // Кросовер
+        string child1 = GeneticAlgorythm.Crossover(parent1, parent2);
+        string child2 = GeneticAlgorythm.Crossover(parent1, parent2);
+
+        // Мутація
+        GeneticAlgorythm.Mutate(ref child1, mutationRate);
+        GeneticAlgorythm.Mutate(ref child2, mutationRate);
+
+        newPopulation.Add(child1);
+        newPopulation.Add(child2);
+    }
+
+    population = newPopulation;
+}
+
+// Знаходження найкращого розв'язку в останній популяції
+
+double bestX = 0;
+int bestXIndex = 0;
+double bestFitness = 0;
+
+for (int i = 0; i < population.Count; i++)
+{
+    string individual = population[i];
+    double individualRepresantation = GeneticAlgorythm.BinaryToDecimal(individual);
+    double fitness = GeneticAlgorythm.FitnessFunction(individualRepresantation);
+
+    if (bestFitness < fitness)
+    {
+        bestFitness = fitness;
+        bestX = individualRepresantation;
+        bestXIndex = i;
+    }
+}
+
+string bestXBinary = population[bestXIndex];
+
+Console.WriteLine($"Best X index: {bestXIndex}");
+Console.WriteLine($"Best X in binary code: {bestXBinary}");
+Console.WriteLine($"Best X: {bestX}");
+Console.WriteLine($"Fitness value: {bestFitness}");
